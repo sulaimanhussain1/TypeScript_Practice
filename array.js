39bff9c7609f2542bfb3a9134fe1e56ad1b91a6c
Sulaiman Hussain <67717623+sulaimanhussain1@users.noreply.github.com>
2023-01-30 17:25:15 +0500

    This is TypeScript practice file.

---
+++ array.js
@@ -0,0 +1,11 @@
+"use strict";
+// let arr:string[]=["php" , "python" , "java" , "js" , "ts" , "node"];
+// console.log(arr);
+// let random:[string,number,string,boolean] = ["Sulaiman" , 19 , "abc@gmail.com" , true];
+// console.log(random);
+// random[2] = "xyz@gmail.com";
+// console.log(random);
+let random = ["Sulaiman", 19, "abc@gmail.com", true];
+console.log(random);
+random[3] = false;
+console.log(random);
---
+++ array.ts
@@ -0,0 +1,17 @@
+
+// let arr:string[]=["php" , "python" , "java" , "js" , "ts" , "node"];
+// console.log(arr);
+
+
+// let random:[string,number,string,boolean] = ["Sulaiman" , 19 , "abc@gmail.com" , true];
+// console.log(random);
+
+// random[2] = "xyz@gmail.com";
+// console.log(random);
+
+
+let random:any = ["Sulaiman" , 19 , "abc@gmail.com" , true];
+console.log(random);
+
+random[3] = false;
+console.log(random);
