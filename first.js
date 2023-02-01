0ed9626a55fa1ef76ab1905d8d63058e236b7e2c
Sulaiman Hussain <67717623+sulaimanhussain1@users.noreply.github.com>
2023-02-01 17:22:46 +0500

    TypeScript practice files.'

---
+++ first.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let message = "Sulaiman your class starts at 9 pm.";
+exports.default = message;
---
+++ first.ts
@@ -0,0 +1,4 @@
+
+let message : string = "Sulaiman your class starts at 9 pm."
+
+export default message;
