80da18c5357a6c8b91dd2d374cb0a396200fa2d1
Sulaiman Hussain <67717623+sulaimanhussain1@users.noreply.github.com>
2023-02-03 17:05:33 +0500

    TypeScript practice files.

---
+++ function_type.js
@@ -0,0 +1,9 @@
+"use strict";
+// Function Type :- jo bhi value hum function me return karwate hain usko function type kehte hain
+// agr hum koi number return krwaye tw uski type numeric kehlaye gi : agr koi string return krwaye tw uski type string kehlaye gi 
+const a = 10;
+const b = "Sulaiman Hussain";
+function call() {
+    return a;
+}
+call();
---
+++ function_type.ts
@@ -0,0 +1,15 @@
+
+
+// Function Type :- jo bhi value hum function me return karwate hain usko function type kehte hain
+// agr hum koi number return krwaye tw uski type numeric kehlaye gi : agr koi string return krwaye tw uski type string kehlaye gi 
+
+
+const a = 10;
+const b = "Sulaiman Hussain";
+
+function call():number
+{
+    return a;
+}
+
+call();
