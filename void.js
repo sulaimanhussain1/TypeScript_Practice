c2ecd0a3e7e41c08d2eac9abe443c8d9457d1ac6
Sulaiman Hussain <67717623+sulaimanhussain1@users.noreply.github.com>
2023-02-04 11:55:37 +0500

    TypeScript practice file.

---
+++ void.js
@@ -0,0 +1,7 @@
+"use strict";
+//  Void ka mtlb hota hai ek dm blank jo kuch bh return nh kr raha ho 
+let today;
+function printData() {
+    today = new Date;
+}
+printData();
---
+++ void.ts
@@ -0,0 +1,10 @@
+
+//  Void ka mtlb hota hai ek dm blank jo kuch bh return nh kr raha ho 
+
+let today ;
+
+function printData(){
+    today = new Date;
+}
+
+printData();
