80f40b178c727837dfcb12eb98762d93fc3ef646
Sulaiman Hussain <67717623+sulaimanhussain1@users.noreply.github.com>
2023-02-12 18:54:35 +0500

    Practice Files.

---
+++ tuple.js
@@ -0,0 +1,9 @@
+"use strict";
+//   Tuple is just a advanced array ..... Tuple me hr ek array ko specific type define krskte hain.
+//   lkn array k ander batana parega k ye poora string type hai ya numeric 
+let role = ["manager", "admin", 24, "employee"];
+role.push(true);
+role.push(10);
+console.log(role);
+role[1] = "boss";
+console.log(role);
---
+++ tuple.ts
@@ -0,0 +1,13 @@
+ 
+//   Tuple is just a advanced array ..... Tuple me hr ek array ko specific type define krskte hain.
+//   lkn array k ander batana parega k ye poora string type hai ya numeric 
+
+
+let role:[string,string, number, string, boolean? , number? ] = ["manager" , "admin" , 24  , "employee"];
+role.push(true);
+role.push(10);
+
+console.log(role);
+
+role[1] = "boss";
+console.log(role);
