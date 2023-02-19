859a17a569b580b82344c431145d00def0fe4684
Sulaiman Hussain <67717623+sulaimanhussain1@users.noreply.github.com>
2023-02-19 18:34:57 +0500

    Typescript Practice.

---
+++ second.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.name2 = exports.name1 = void 0;
+let name1 = "Jhangeer Hussain";
+exports.name1 = name1;
+let name2 = "Sikander Ali";
+exports.name2 = name2;
---
+++ second.ts
@@ -0,0 +1,6 @@
+
+let name1 : string = "Jhangeer Hussain";
+
+let name2 : string = "Sikander Ali";
+
+export {name1 , name2};
