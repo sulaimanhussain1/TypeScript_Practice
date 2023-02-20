9c0172ad96e6f84960a0e5d547defd2fad1e8425
Sulaiman Hussain <67717623+sulaimanhussain1@users.noreply.github.com>
2023-02-20 17:02:16 +0500

    Typescript Practice.

---
+++ object.js
@@ -0,0 +1,13 @@
+"use strict";
+const person = {
+    name1: "Sulaiman Hussain",
+    age: 19,
+    email: "hussainsulaiman@gmail.com"
+};
+const person1 = {
+    name1: "Jhangeer Hussain",
+    age: 30,
+    email: "hussainjhangeer@gmail.com",
+};
+console.log(person.name1);
+console.log(person1.email);
---
+++ object.ts
@@ -0,0 +1,18 @@
+type objType = {name1:string , age:number , email: string };
+
+
+const person : objType = {
+    name1 : "Sulaiman Hussain",
+    age : 19, 
+    email : "hussainsulaiman@gmail.com"
+}; 
+
+
+const person1 : objType = {
+    name1 : "Jhangeer Hussain",
+    age : 30, 
+    email : "hussainjhangeer@gmail.com",
+}; 
+
+console.log(person.name1);
+console.log(person1.email);
