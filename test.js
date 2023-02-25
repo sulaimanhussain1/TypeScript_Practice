3f7e23329348e353370cf879049f3b0b0ae086fa
Sulaiman Hussain <67717623+sulaimanhussain1@users.noreply.github.com>
2023-02-25 17:28:05 +0500

    Typescript Practice.

---
+++ test.js
@@ -0,0 +1,24 @@
+"use strict";
+// let num :number = 10;
+// console.log(num);
+// ########################### Function In TypeScript ########################
+// function mobName(mob:string){
+//     return mob;
+// }
+// let User = prompt("Your Mobile Name ? : ","Laptop");
+// let result = mobName("Your Mobile is " + User);
+// console.log(result);
+// ################################# Same function In javascript ########################
+// function add(num1, num2){
+//     if(typeof num1 === "number" && typeof num2 === "number"){
+//         return num1 + num2 ;
+//     }
+//     else{
+//         return new Error ("This value is not a number.");
+//     }
+// }
+// console.log(add(10,22));
+// ############################ Javascript Vs Typescript ##########################
+// Typescript me hume hm code likhna hoga or humare saare k saare error compile time pr check hojayenge 
+// ######################### Type Inference #################################
+// khud se idea laga lena k kis type ka data store huwa hai variable me 
